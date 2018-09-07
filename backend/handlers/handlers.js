9da4ed0cb02035dd639fffa975206037e7568703
Kirill Kuznetcov <raptor@MacBook-Pro-Kirill-2.local>
2018-09-08 01:41:02 +0200

    fix

--- backend/handlers/handlers.js
+++ backend/handlers/handlers.js
@@ -1,11 +1,11 @@
 const db = require('./../database/database');
-const keyboars = require('./../requests/keyboards/keyboards');
+const keyboards = require('./../requests/keyboards/keyboards');
 const request = require('./../requests/requests');
 
 function createUser(req, res) {
     const body = req.params;
     db.user.create(body.userId, body.publicKey);
-    request(body.userId, '👾 Now you can create vote or take part in vote!', keyboars.main);
+    request.sendKeyboard(body.userId, '👾 Now you can create vote or take part in vote!', keyboards.main);
     res.end();
 }
 
